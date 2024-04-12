9cd4c881f800d6efbeb3482f4a99da0ff3755907
Chloe Zermatten <chloezermatten@gmail.com>
2024-04-12 16:14:35 +0100

    feat: set up db connexion

--- index.ts
+++ index.ts
@@ -3,10 +3,34 @@ const app = express()
 const port = 3000
 const morgan = require('morgan')
 const cors = require('cors')
+const { DataSource } = require("typeorm")
+require('dotenv').config()
 
 app.use(morgan("dev"))
 app.use(cors())
 
+const dataSource = new DataSource({
+    type: "postgres",
+    database: "lesson-booking",
+    url: process.env.POSTGRES_URL,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER, 
+    password: process.env.POSTGRES_PASSWORD
+})
+
+dataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
+
+module.exports = {
+    app, 
+    dataSource
+}
