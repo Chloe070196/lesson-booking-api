c1a6b7e0fde0e2329a9274c31f9593ea398bf714
Chloe Zermatten <chloezermatten@gmail.com>
2024-04-27 16:21:00 +0100

    fix: string typos/inconsistencies

--- __tests__/input.validation.spec.js
+++ __tests__/input.validation.spec.js
@@ -4,16 +4,16 @@ import { InputValidation } from "../src/middlewares/input_validation";
 describe("input validation: ", () => {
   const inputValidation = new InputValidation();
 
-  describe("validateInputFiled should return", () => {
-    test(" false when the value is an empty string: ", () => {
+  describe("validateInputField should return", () => {
+    test(" false when the value is an empty string", () => {
         const result = inputValidation.validateInputField("") 
         expect(result).toEqual(false);
     });
-    test("validateInputFiled should return false when the value is null or undefined: ", () => {
+    test(" false when the value is null or undefined", () => {
         const result = inputValidation.validateInputField(undefined) 
         expect(result).toEqual(false);
     });
-    test("validateInputFiled should return true when the value is null or undefined: ", () => {
+    test(" true when the value is null or undefined", () => {
         const result = inputValidation.validateInputField("some string") 
         expect(result).toEqual(true);
     });
