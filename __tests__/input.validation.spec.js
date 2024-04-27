66e4da77af55eafe9fa10644902e8a3074148967
Chloe Zermatten <chloezermatten@gmail.com>
2024-04-27 16:36:22 +0100

    test: findInvalidInput

--- __tests__/input.validation.spec.js
+++ __tests__/input.validation.spec.js
@@ -6,16 +6,32 @@ describe("input validation: ", () => {
 
   describe("validateInputField should return", () => {
     test(" false when the value is an empty string", () => {
-        const result = inputValidation.validateInputField("") 
-        expect(result).toEqual(false);
+      const result = inputValidation.validateInputField("");
+      expect(result).toEqual(false);
     });
     test(" false when the value is null or undefined", () => {
-        const result = inputValidation.validateInputField(undefined) 
-        expect(result).toEqual(false);
+      const result = inputValidation.validateInputField(undefined);
+      expect(result).toEqual(false);
     });
     test(" true when the value is null or undefined", () => {
-        const result = inputValidation.validateInputField("some string") 
-        expect(result).toEqual(true);
+      const result = inputValidation.validateInputField("some string");
+      expect(result).toEqual(true);
+    });
+  });
+  describe("findInvalidField should return", () => {
+    test(" the first invalid field it finds", () => {
+      const result = inputValidation.findInvalidField({
+        name: "joe",
+        email: undefined,
+      });
+      expect(result).toEqual("email");
+    });
+    test(" undefined when all fields are valid", () => {
+      const result = inputValidation.findInvalidField({
+        name: "joe",
+        email: "test@email.com",
+      });
+      expect(result).toEqual(undefined);
     });
   });
 });
