8a179093474677a1f419a9e55f4378163b123c57
Chloe Zermatten <chloezermatten@gmail.com>
2024-04-30 17:40:25 +0100

    fix: app uses express body parser

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express, { Request, Response } from "express";
 const app = express();
 import morgan from "morgan";
 import cors from "cors";
+import { userRouter } from "./routes/user/index.ts";
 
 app.use(morgan("dev"));
 app.use(cors());
+app.use(express.json())
 
 //TODO: add routes here
+app.use('/user', userRouter)
 
 // deals with any attempt to access an non-existent route
 app.get("/*", (req: Request, res: Response) => {
