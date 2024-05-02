9769f3aa7d74be4e7dea64dd16bab3792f9a1c40
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-02 17:27:55 +0100

    fix: type check in query builder

--- src/helpers/typeorm-querybuilder.ts
+++ src/helpers/typeorm-querybuilder.ts
@@ -1,14 +1,17 @@
-import { EntityTarget } from "typeorm"
+import { EntityTarget } from "typeorm";
+import { isNotStringType } from "../utils/checkDataType.ts";
 
 export class CrudRepositoryHelpers<T> {
-    buildIntoAlias( entity: EntityTarget<T>) {  
-        return entity.toString()
+  buildIntoAlias(entity: EntityTarget<T>) {
+    if (isNotStringType(entity)) {
+      return entity.name.toLowerCase();
     }
-    
-    buildWhereConditionString(entity: EntityTarget<T>, condition: Object) {
-        const alias = this.buildIntoAlias(entity)
-        const conditionKey = Object.keys(condition)[0].toLowerCase()
-        const conditionStr = alias + '.' + conditionKey + ' = :' + conditionKey
-        return conditionStr
-    }
+    throw new Error("error building alias - incorrect type");
+  }
+
+  buildWhereConditionString(entity: EntityTarget<T>, condition: Object) {
+    const alias = this.buildIntoAlias(entity);
+    const conditionKey = Object.keys(condition)[0].toLowerCase();
+    return alias + "." + conditionKey + " = :" + conditionKey;
+  }
 }
