358154ad6bc57a05011301ec3235511a67632f82
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-02 17:24:51 +0100

    feat: user registration repository

---
+++ src/repositories/user/index.ts
@@ -0,0 +1,21 @@
+import { DataSource, ObjectLiteral } from "typeorm";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+import { User } from "../../entities/user/index.js";
+import { CrudRepository } from "../crud-repository/index.ts";
+import { CrudRepositoryHelpers } from "../../helpers/typeorm-querybuilder.ts";
+
+export class UserRepository extends CrudRepository<User> {
+  constructor(
+    dataSource: DataSource,
+    helpers: CrudRepositoryHelpers<User>,
+  ) {
+    super(dataSource, helpers);
+  }
+
+  async getUserBy(uniqueProperty: Object): Promise< void | ObjectLiteral> {
+    return await this.read(User, uniqueProperty);
+  }
+  async post(user: QueryDeepPartialEntity<User>) {
+    return await this.create(user, User)
+  }
+}
