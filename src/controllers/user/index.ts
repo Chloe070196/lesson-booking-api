bbd78bd47683eac04bd37239626ed0f7b1bed09a
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-02 17:29:27 +0100

    fix: correct import path

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { UserService } from "../../services/UserService.ts";
+import { UserService } from "../../services/user/index.ts";
 import { sendDataResponse, sendMessageResponse } from "../../utils/responses.ts";
 
 export class UserController { 
