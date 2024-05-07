187e819ead51879a4dbf826a9e3fd400600ddc06
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-07 11:31:27 +0100

    feat: remove sensitive info

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -2,13 +2,20 @@ import bcrypt from "bcrypt";
 import { UserRepository } from "../../repositories/user/index.ts";
 import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
 import { User } from "../../entities/user/index.js";
+import { ObjectLiteral } from "typeorm";
 
 export class UserService {
-
   constructor(private userRepository: UserRepository) {}
 
   hash(string: string) {
     return bcrypt.hash(string, 8);
+  }
+
+  removeSensitiveInfo(obj: ObjectLiteral, fieldNameList: Array<string>) {
+    fieldNameList.forEach((fieldName) => {
+      delete obj[fieldName];
+    });
+    return obj;
   }
 
   async register(user: User) {
@@ -20,7 +27,7 @@ export class UserService {
     };
     return await this.userRepository.post(partialUser);
   }
-  
+
   async doesUserExist(email: string) {
     const foundUser = await this.userRepository.getUserBy({ email });
     return !!foundUser;
