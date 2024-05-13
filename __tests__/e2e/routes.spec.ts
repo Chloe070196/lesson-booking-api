fdec55fc572e8f640d66de1a93891b57198f5a07
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-13 13:31:32 +0100

    test: user login happy path

--- __tests__/e2e/routes.spec.ts
+++ __tests__/e2e/routes.spec.ts
@@ -1,43 +1,62 @@
 import mockClient from "supertest";
 import TestUtils from "../utils.ts";
 
-describe("/register : POST", () => {
-  
+describe("user/", () => {
   beforeAll(async () => {
-    new TestUtils()
-    await TestUtils.initializeApp()
-  })
-
-  test(" if the user is new, saves their details into the database and return a success (201) response containing their id and username in its data object", async () => {
-    const mockUser = {
-      password: process.env.TEST_USER_THREE_PASSWORD,
-      username: "Danny1234",
-      email: process.env.TEST_USER_THREE_EMAIL,
-    };
-    const response = await mockClient(TestUtils.app)
-      .post("/user/register")
-      .send(mockUser);
-
-    expect(response.status).toEqual(201);
-    expect(response.body).toHaveProperty("data");
-    expect(response.body.data).toHaveProperty("id");
-    expect(response.body.data.username).toEqual("Danny1234");
+    new TestUtils();
+    await TestUtils.initializeApp();
   });
 
-  test(" if the user not new, sends a 400 error message", async () => {
-    const mockUser = {
-      password: process.env.TEST_USER_THREE_PASSWORD,
-      username: "Danny1234",
-      email: process.env.TEST_USER_THREE_EMAIL,
-    };
-    const response = await mockClient(TestUtils.app)
-      .post("/user/register")
-      .send(mockUser);
+  describe("/register : POST", () => {
+    test(" if the user is new, saves their details into the database and return a success (201) response containing their id and username in its data object", async () => {
+      const mockUser = {
+        password: process.env.TEST_USER_THREE_PASSWORD,
+        username: "Danny1234",
+        email: process.env.TEST_USER_THREE_EMAIL,
+      };
+      const response = await mockClient(TestUtils.app)
+        .post("/user/register")
+        .send(mockUser);
 
-    expect(response.status).toEqual(400);
-    expect(response.body).toHaveProperty("message");
-    expect(response.body.message).toEqual("The email provided is already in use");
-    expect(response.body).toHaveProperty("status");
-    expect(response.body.status).toEqual("fail");
+      expect(response.status).toEqual(201);
+      expect(response.body).toHaveProperty("data");
+      expect(response.body.data).toHaveProperty("id");
+      expect(response.body.data.username).toEqual("Danny1234");
+    });
+
+    test(" if the user not new, sends a 400 error message", async () => {
+      const mockUser = {
+        password: process.env.TEST_USER_THREE_PASSWORD,
+        username: "Danny1234",
+        email: process.env.TEST_USER_THREE_EMAIL,
+      };
+      const response = await mockClient(TestUtils.app)
+        .post("/user/register")
+        .send(mockUser);
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toHaveProperty("message");
+      expect(response.body.message).toEqual(
+        "The email provided is already in use"
+      );
+      expect(response.body).toHaveProperty("status");
+      expect(response.body.status).toEqual("fail");
+    });
+  });
+
+  describe("/login :POST", () => {
+    test(" if the credentials match, returns a jwt", async () => {
+      const mockUser = {
+        password: process.env.TEST_USER_THREE_PASSWORD,
+        email: process.env.TEST_USER_THREE_EMAIL,
+      };
+      const response = await mockClient(TestUtils.app)
+        .post("/user/login")
+        .send(mockUser);
+
+      expect(response.status).toEqual(201);
+      expect(response.body).toHaveProperty("data");
+      expect(response.body.data).toHaveProperty("token");
+    });
   });
 });
