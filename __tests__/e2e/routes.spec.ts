ec0be7d4915cfe19b9ae2ac85e31897ed4e28a95
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-13 13:24:57 +0100

    test: user registration unhappy path

--- __tests__/e2e/routes.spec.ts
+++ __tests__/e2e/routes.spec.ts
@@ -18,5 +18,23 @@ describe("/register : POST", () => {
 
     expect(response.status).toEqual(201);
     expect(response.body).toHaveProperty("data");
+    expect(response.body.data).toHaveProperty("id");
+    expect(response.body.data.username).toEqual("Danny1234");
+  });
+  test(" if the user not new, sends a 400 error message", async () => {
+    const mockUser = {
+      password: process.env.TEST_USER_THREE_PASSWORD,
+      username: "Danny1234",
+      email: process.env.TEST_USER_THREE_EMAIL,
+    };
+    const response = await mockClient(TestUtils.app)
+      .post("/user/register")
+      .send(mockUser);
+
+    expect(response.status).toEqual(400);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toEqual("The email provided is already in use");
+    expect(response.body).toHaveProperty("status");
+    expect(response.body.status).toEqual("fail");
   });
 });
