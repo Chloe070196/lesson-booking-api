db6e7ff668caec26829369a6109dfa88517fc295
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-13 12:51:42 +0100

    refactor: move app.use(docs) to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import { Request, Response } from 'express';
 import { dataSource } from './dataSource.ts'
 import { app } from './server.ts';
+
+// allows for .env variables to be used througout the app.
 import dotenv from 'dotenv'
 dotenv.config()
 
-const port = 3000;
+// enable swagger UI at <host>:<port>/api-docs
+import { docsRouter } from "./routes/api-docs/index.ts";
+app.use('/api-docs', docsRouter)
 
+
+// enable typeOrm dataSource
 dataSource.initialize()
 .then(() => {
   console.log("Data Source has been initialized!")
@@ -13,8 +20,15 @@ dataSource.initialize()
   console.error("Error during Data Source initialization", err)
 })
 
+// set up the server to list for requests
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+// deals with any attempt to access an non-existent route
+app.get("/*", (req: Request, res: Response) => {
+  res.status(404).json({ status: "fail", message: "not found" });
 });
 
 export { app }
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,13 @@
-import express, { Request, Response } from "express";
+import express from "express";
 const app = express();
 import morgan from "morgan";
 import cors from "cors";
 import { userRouter } from "./routes/user/index.ts";
-import { docsRouter } from "./routes/api-docs/index.ts";
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json())
 
-//TODO: add routes here
-app.use('/api-docs', docsRouter)
 app.use('/user', userRouter)
-
-// deals with any attempt to access an non-existent route
-app.get("/*", (req: Request, res: Response) => {
-  res.status(404).json({ status: "fail", message: "not found" });
-});
 
 export { app };
