194be51e0a78f32eb6486e056bd373e32099bd72
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-15 17:26:41 +0100

    style: remove uncesseray indentations

--- src/repositories/teacher/index.ts
+++ src/repositories/teacher/index.ts
@@ -3,11 +3,8 @@ import { CrudRepositoryHelpers } from "../../helpers/typeorm-querybuilder";
 import { Teacher } from "../../entities/teacher";
 import { CrudRepository } from "../crud-repository";
 
-export default class TeacherRepository extends CrudRepository<Teacher>{
-        constructor(
-          dataSource: DataSource,
-          helpers: CrudRepositoryHelpers<Teacher>,
-        ) {
-          super(dataSource, helpers);
-        }
+export default class TeacherRepository extends CrudRepository<Teacher> {
+  constructor(dataSource: DataSource, helpers: CrudRepositoryHelpers<Teacher>) {
+    super(dataSource, helpers);
+  }
 }
