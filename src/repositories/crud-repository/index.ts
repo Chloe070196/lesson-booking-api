f2e2586b406d964e9e3027b05d5b37575298d0e1
Chloe Zermatten <chloezermatten@gmail.com>
2024-05-16 10:29:01 +0100

    refactor: extract insert and readOneBy from create

--- src/repositories/crud-repository/index.ts
+++ src/repositories/crud-repository/index.ts
@@ -1,4 +1,4 @@
-import { DataSource, ObjectType } from "typeorm";
+import { DataSource, FindOptionsWhere, ObjectType } from "typeorm";
 import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
 import { CrudRepositoryHelpers } from "../../helpers/typeorm-querybuilder";
 
@@ -11,14 +11,17 @@ export class CrudRepository<T> {
     this.helpers = helpers;
   }
 
-  async create(values: QueryDeepPartialEntity<T>, entity: ObjectType<T>) {
-    const res = await this.dataSource.manager.insert(entity, values)
-    const id = res.identifiers[0].id
-    const result = await this.dataSource.manager.findOneBy(entity, id)
+  async insert(values: QueryDeepPartialEntity<T>, entity: ObjectType<T>) {
+    const res = await this.dataSource.getRepository(entity).insert(values)
+    return res
+  }
+
+  async readOneBy(entity: ObjectType<T>, condition: FindOptionsWhere<T>) {
+    const result = await this.dataSource.getRepository(entity).findOneBy(condition)
     return result
   }
 
-  async read(entity: ObjectType<T>, condition: Object) {
+  async readOneIncludeAllProperties(entity: ObjectType<T>, condition: Object) {
     const alias = this.helpers.buildIntoAlias(entity);
     const conditionStr = this.helpers.buildWhereConditionString(
       entity,
